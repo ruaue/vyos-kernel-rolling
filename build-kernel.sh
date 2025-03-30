150cfd8c43e66ac2656f4afa9e28636ae6dfb147
debiansid <9624961+debiansid@users.noreply.github.com>
2025-03-30 08:18:34 +0800

    Update build-kernel.sh

--- build-kernel.sh
+++ build-kernel.sh
@@ -18,7 +18,7 @@ echo "I: clean modified files"
 git reset --hard HEAD
 
 KERNEL_VERSION=$(make kernelversion)
-KERNEL_SUFFIX=-vyos
+KERNEL_SUFFIX=-$(dpkg --print-architecture)-vyos
 
 # VyOS requires some small Kernel Patches - apply them here
 # It's easier to habe them here and make use of the upstream
